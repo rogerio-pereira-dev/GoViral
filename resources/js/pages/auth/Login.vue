c485608512dc055ada07dba511506b8f102816f9
Rogerio Pereira <hi@rogeriopereira.dev>
2026-02-27 01:34:42 -0500

    fix(app): Build after removing Register Route

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -98,13 +98,13 @@ defineProps<{
                 </Button>
             </div>
 
-            <div
+            <!-- <div
                 class="text-center text-sm text-muted-foreground"
                 v-if="canRegister"
             >
                 Don't have an account?
                 <TextLink :href="register()" :tabindex="5">Sign up</TextLink>
-            </div>
+            </div> -->
         </Form>
     </AuthBase>
 </template>
